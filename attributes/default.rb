62d341a0a677769a64ab7ef5cb24bb2546fbde79
Christian Fischer <sw-dev@computerlyrik.de>
2015-01-16 01:17:06 +0100

    some simplification in attributes to construct the url

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,9 +1,11 @@
-default[:openfire][:source_tarball] = 'openfire_3_8_1.tar.gz'
+default[:openfire][:version] = '3.9.3'
+default[:openfire][:source_tarball] = "openfire_#{openfire[:version].gsub(".","_")}.tar.gz"
 
 # precalculated checksums: `sha256sum openfire_v_v_v.tar.gz | cut -c1-16`
 checksums = {
 	'openfire_3_8_1.tar.gz' => '554dce3a1b0a0b88',
-	'openfire_3_8_0.tar.gz' => 'd5bef61a313ee41b'
+	'openfire_3_8_0.tar.gz' => 'd5bef61a313ee41b',
+	'openfire_3_9_3.tar.gz' => '17e636ddcba3caa039076a63914cec22cfbc67b6af2ec0bc7d22082aded539ed'
 }
 
 default[:openfire][:source_checksum] = checksums[openfire[:source_tarball]]

53bb52ad9c84c14c23d32ac68beb1904b266e0a8
Christian Fischer <sw-dev@computerlyrik.de>
2015-01-17 05:36:15 +0100

    fixing foodcritic issues with node attributes access

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,5 +1,5 @@
-default[:openfire][:version] = '3.9.3'
-default[:openfire][:source_tarball] = "openfire_#{openfire[:version].gsub(".","_")}.tar.gz"
+default['openfire']['version'] = '3.9.3'
+default['openfire']['source_tarball'] = "openfire_#{openfire['version'].gsub(".","_")}.tar.gz"
 
 # precalculated checksums: `sha256sum openfire_v_v_v.tar.gz | cut -c1-16`
 checksums = {
@@ -8,20 +8,20 @@ checksums = {
 	'openfire_3_9_3.tar.gz' => '17e636ddcba3caa039076a63914cec22cfbc67b6af2ec0bc7d22082aded539ed'
 }
 
-default[:openfire][:source_checksum] = checksums[openfire[:source_tarball]]
+default['openfire']['source_checksum'] = checksums[openfire['source_tarball']]
 
-default[:openfire][:base_dir] = '/opt'
-default[:openfire][:home_dir] = "#{openfire[:base_dir]}/openfire"
-default[:openfire][:log_dir] = '/var/log/openfire'
+default['openfire']['base_dir'] = '/opt'
+default['openfire']['home_dir'] = "#{openfire['base_dir']}/openfire"
+default['openfire']['log_dir'] = '/var/log/openfire'
 
-default[:openfire][:user] = 'openfire'
-default[:openfire][:group] = 'openfire'
+default['openfire']['user'] = 'openfire'
+default['openfire']['group'] = 'openfire'
 
-default[:openfire][:pidfile] = '/var/run/openfire.pid'
+default['openfire']['pidfile'] = '/var/run/openfire.pid'
 
 # by default, only enable secure admin port
-default[:openfire][:config][:admin_console][:port] = 9090
-default[:openfire][:config][:admin_console][:secure_port] = 9091
+default['openfire']['config']['admin_console']['port'] = 9090
+default['openfire']['config']['admin_console']['secure_port'] = 9091
 
-default[:openfire][:config][:locale] = 'en'
-default[:openfire][:config][:network][:interface] = nil
+default['openfire']['config']['locale'] = 'en'
+default['openfire']['config']['network']['interface'] = nil
--- recipes/default.rb
+++ recipes/default.rb
@@ -1,50 +1,50 @@
 include_recipe "java::default"
 
-if node[:openfire][:database][:active]
+if node['openfire']['database']['active']
   include_recipe "openfire::database"
 end
 
-group node[:openfire][:group] do
+group node['openfire']['group'] do
   system true
 end
 
-user node[:openfire][:user] do
-  gid node[:openfire][:group]
-  home node[:openfire][:home_dir]
+user node['openfire']['user'] do
+  gid node['openfire']['group']
+  home node['openfire']['home_dir']
   system true
   shell '/bin/sh'
 end
 
-local_tarball_path = "#{Chef::Config[:file_cache_path]}/#{node[:openfire][:source_tarball]}"
+local_tarball_path = "#{Chef:'config'['file_cache_path']}/#{node['openfire']['source_tarball']}"
 
 remote_file local_tarball_path do
-  checksum node[:openfire][:source_checksum]
-  source "http://www.igniterealtime.org/downloadServlet?filename=openfire/#{node[:openfire][:source_tarball]}"
+  checksum node['openfire']['source_checksum']
+  source "http://www.igniterealtime.org/downloadServlet?filename=openfire/#{node['openfire']['source_tarball']}"
 end
 
 bash "install_openfire" do
-  cwd node[:openfire][:base_dir]
+  cwd node['openfire']['base_dir']
   code <<-EOH
     tar xzf #{local_tarball_path}
-    chown -R #{node[:openfire][:user]}:#{node[:openfire][:group]} #{node[:openfire][:home_dir]}
-    mv #{node[:openfire][:home_dir]}/conf /etc/openfire
+    chown -R #{node['openfire']['user']}:#{node['openfire']['group']} #{node['openfire']['home_dir']}
+    mv #{node['openfire']['home_dir']}/conf /etc/openfire
     rm /etc/openfire/openfire.xml
-    mv #{node[:openfire][:home_dir]}/logs /var/log/openfire
-    mv #{node[:openfire][:home_dir]}/resources/security /etc/openfire
+    mv #{node['openfire']['home_dir']}/logs /var/log/openfire
+    mv #{node['openfire']['home_dir']}/resources/security /etc/openfire
   EOH
-  creates node[:openfire][:home_dir]
+  creates node['openfire']['home_dir']
 end
 
 # link to LSB-recommended directories
-link "#{node[:openfire][:home_dir]}/conf" do
+link "#{node['openfire']['home_dir']}/conf" do
   to '/etc/openfire'
 end
 
-link "#{node[:openfire][:home_dir]}/logs" do
+link "#{node['openfire']['home_dir']}/logs" do
   to '/var/log/openfire'
 end
 
-link "#{node[:openfire][:home_dir]}/resources/security" do
+link "#{node['openfire']['home_dir']}/resources/security" do
   to '/etc/openfire/security'
 end
 
@@ -70,7 +70,7 @@ end
 # make a symlink so that openfirectl is happy
 link '/etc/sysconfig' do
   to '/etc/default'
-  only_if { node[:platform_family] == 'debian' }
+  only_if { node['platform_family'] == 'debian' }
 end
 
 template '/etc/sysconfig/openfire' do
@@ -83,8 +83,8 @@ service "openfire" do
   action [ :enable, :start ]
 end
 
-admin_console = node[:openfire][:config][:admin_console]
-admin_port = (admin_console[:secure_port] == -1)? admin_console[:port] : admin_console[:secure_port]
+admin_console = node['openfire']['config']['admin_console']
+admin_port = (admin_console['secure_port'] == -1)? admin_console['port'] : admin_console['secure_port']
 log "And now visit the server on :#{admin_port} to run the openfire wizard." do
   action :nothing
 end
